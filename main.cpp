fa47063c3e91c9d63ffa8d6efa0ac47cfd3d5167
garrett1001 <31227173+garrett1001@users.noreply.github.com>
2017-10-04 17:38:31 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "bitmap.h"
+#include <vector>
+#include <string>
+
+Int main()
+{
+//ask for file name
+//convert file into grid
+//create loop to cange each pixel to grey by changing the value of the rgb color
+//saveas oldtimey.bmp
+
+
+
+    return 0;
+}
