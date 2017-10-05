d67353c2df55fdd71714039db600053fa476aa2e
garrett1001 <gwelton@mail.csuchico.edu>
2017-10-04 18:21:33 -0700

    Opens image and converts into matrix

--- main.cpp
+++ main.cpp
@@ -2,12 +2,23 @@
 #include <vector>
 #include <string>
 
-Int main()
+using namespace std;
+
+int main()
 {
+Bitmap image;
+vector <vector <Pixel> > bmp;
+Pixel rgb;
 //ask for file name
-//convert file into grid
-//create loop to cange each pixel to grey by changing the value of the rgb color
-//saveas oldtimey.bmp
+image.open("machupicchu.bmp");
+bmp = image.toPixelMatrix();
+//find file size
+cout<<"machupicchu.bmp has been loaded. It is "<<bmp[0].size()<<" pixels wide and"<<bmp.size()<<"pixels high"<<endl;
+//convert file into matrix
+//create loop that goes through the rows and thin one within that goes through the colomns
+//change each pixel to grey by averaging the values of the rgb color
+//end loop once it goes through all rows and colomns
+//save as oldtimey.bmp
 
 
 
